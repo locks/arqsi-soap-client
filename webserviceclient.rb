c54aa942ef2ba7938eae88c513bbc9ac012506be
locks <rokusu@gmail.com>
2009-12-11 10:04:38 +0000

    new shit

--- webserviceclient.rb
+++ webserviceclient.rb
@@ -6,14 +6,18 @@ Savon::SOAP.version = 2
 url = "http://dot.dei.isep.ipp.pt/060516/dir1/srvARQSI45.asmx?wsdl"
 
 get '/' do
-=begin
+
   client = Savon::Client.new url
-  client.wsdl.soap_actions
-=end
-  "<p>P&aacute;gina do grupo 45</p>" +
   
-  '<a href="servico/hello_world">Hello World</a><br />' +
-  '<a href="servico/inserir_pontuacao">Inserir Pontuacao</a>'
+  "<p>P&aacute;gina de testes dos serviços do grupo 45</p>" + client.wsdl.soap_actions.keys.to_s +
+  
+  client.wsdl.soap_actions.keys.each do |key|
+    key.to_s + "<br />" + "TATATATA"
+  end.to_s +
+=begin
+=end
+  '<br /><br />'+
+  '<a href="servico/hello_world">Hello World</a><br />' '<a href="servico/inserir_pontuacao">Inserir Pontuacao</a>'
 end
 
 get '/servico/:nome' do
